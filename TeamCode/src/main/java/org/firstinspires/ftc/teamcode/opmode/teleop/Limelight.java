d6ff1469ba3801d00b6513913c86a65b70bdefb5
PythonMaster245 <connorlithgow@4hscream.org>
2025-09-14 14:29:54 -0500

    Added driving to limelight test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Limelight.java
@@ -1,10 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.Network;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import java.util.List;
 
@@ -12,9 +20,28 @@ import java.util.List;
 public class Limelight extends OpMode {
 
     private Limelight3A limelight;
+    GamepadEx chassis;
+    IMU imu;
+    DcMotor frontLeftMotor;
+    DcMotor frontRightmotor;
+    DcMotor backLeftmotor;
+    DcMotor backRightmotor;
 
     @Override
     public void init() {
+
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        frontRightmotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backLeftmotor = hardwareMap.dcMotor.get("backLeftMotor");
+        backRightmotor = hardwareMap.dcMotor.get("backRightMotor");
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftmotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        chassis = new GamepadEx(gamepad1);
+
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.pipelineSwitch(2);
@@ -23,32 +50,25 @@ public class Limelight extends OpMode {
 
     @Override
     public void loop(){
+
+        chassis.readButtons();
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x * 1.1;
+        double rx = gamepad1.right_stick_x;
+
+        frontLeftMotor.setPower(y + x + rx);
+        backLeftmotor.setPower(y - x + rx);
+        frontRightmotor.setPower(y - x - rx);
+        backRightmotor.setPower(y + x - rx);
+
         LLResult result = limelight.getLatestResult();
         List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
         int id = 0;
         for (LLResultTypes.FiducialResult fiducial : fiducials) {
             id = fiducial.getFiducialId(); // The ID number of the fiducial
-            double x = fiducial.getTargetXDegrees(); // Where it is (left-right)
-            double y = fiducial.getTargetYDegrees(); // Where it is (up-down)
+            double limelightx = fiducial.getTargetXDegrees(); // Where it is (left-right)
+            double limelighty = fiducial.getTargetYDegrees(); // Where it is (up-down)
             double StrafeDistance_3D = fiducial.getRobotPoseTargetSpace().getPosition().y;
-            NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-            NetworkTableEntry ty = table.getEntry("ty");
-            double targetOffsetAngle_Vertical = ty.getDouble(0.0);
-
-            // how many degrees back is your limelight rotated from perfectly vertical?
-            double limelightMountAngleDegrees = 25.0;
-
-            // distance from the center of the Limelight lens to the floor
-            double limelightLensHeightInches = 20.0;
-
-            // distance from the target to the floor
-            double goalHeightInches = 60.0;
-
-            double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-            double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
-
-            //calculate distance
-            double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
 
             telemetry.addData("Fiducial " + id, "is " + StrafeDistance_3D + " meters away");
         }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MotorTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Motor Direction Test")
+public class MotorTest extends OpMode {
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+    @Override
+    public void init(){
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop(){
+        if(gamepad1.a){
+            frontLeftMotor.setPower(1);
+        }
+        if(gamepad1.b){
+            frontRightMotor.setPower(1);
+        }
+        if(gamepad1.x){
+            backLeftMotor.setPower(1);
+        }
+        if(gamepad1.y){
+            backRightMotor.setPower(1);
+        }
+    }
+}
