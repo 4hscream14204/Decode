25f98c28858fac33e41430f9dcb9449f292b48fb
PythonMaster245 <connorlithgow@4hscream.org>
2025-09-20 15:16:45 -0500

    Added launcher subsystem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/RobotBase.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode.robotbase;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Launcher;
 
 public class RobotBase {
 
         public Intake intakeSubsystem;
+        public Launcher launcherSubsystem;
 
         public RobotBase(HardwareMap hwMap) {
 
             intakeSubsystem = new Intake(hwMap.dcMotor.get("intake_motor"));
+            launcherSubsystem = new Launcher(hwMap.get(DcMotorEx.class, "launcherMotor"));
         }
 }
