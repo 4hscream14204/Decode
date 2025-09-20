52eb3cb34136914e9c5b14b71a82c094fcc33888
AizahR <aizrizwan16@gamil.com>
2025-09-20 16:50:01 -0500

    tested launcher in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/SchematicannonTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/SchematicannonTeleOp.java
@@ -20,7 +20,7 @@ import org.firstinspires.ftc.teamcode.subsystems.Intake;
 
 import java.util.List;
 
-@TeleOp(name = "AlignToBall")
+@TeleOp(name = "Schematic Cannon")
 public class SchematicannonTeleOp extends OpMode {
 
     RobotBase robotBase;
@@ -44,7 +44,7 @@ public class SchematicannonTeleOp extends OpMode {
     public void init() {
 
         robotBase = new RobotBase(hardwareMap);
-        robotBase.chassisSubsystem.initLimelight();
+        //robotBase.chassisSubsystem.initLimelight();
         /*frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         frontRightmotor = hardwareMap.dcMotor.get("frontRightMotor");
         backLeftmotor = hardwareMap.dcMotor.get("backLeftMotor");
@@ -54,10 +54,10 @@ public class SchematicannonTeleOp extends OpMode {
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftmotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftmotor.setDirection(DcMotorSimple.Direction.REVERSE);*/
         chassis = new GamepadEx(gamepad1);
 
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+       /* limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.pipelineSwitch(0);
         limelight.start(); // This tells Limelight to start looking!*/
@@ -98,9 +98,9 @@ public class SchematicannonTeleOp extends OpMode {
     public void loop() {
         chassis.readButtons();
 
-        robotBase.chassisSubsystem.updateLimelight();
+       // robotBase.chassisSubsystem.updateLimelight();
 
-        robotBase.chassisSubsystem.drive(chassis.getLeftY(), chassis.getLeftX(), chassis.getRightX(), bolTurnToArtifact);
+       /* robotBase.chassisSubsystem.drive(chassis.getLeftY(), chassis.getLeftX(), chassis.getRightX(), bolTurnToArtifact);*/
 
         //LLResult result = limelight.getLatestResult();
         /*List<LLResultTypes.ColorResult> fiducials = result.getColorResults();
@@ -130,11 +130,11 @@ public class SchematicannonTeleOp extends OpMode {
         backRightmotor.setPower(y + x - rx); */
 
         // telemetry.addData("id", id);
-        telemetry.addData("tx", robotBase.chassisSubsystem.getTargetX());
-        telemetry.addData("ty", robotBase.chassisSubsystem.getTargetY());
-        telemetry.addData("ta", robotBase.chassisSubsystem.getTargetArea());
-        telemetry.addData("Pipeline type", robotBase.chassisSubsystem.getPiplineType());
-        telemetry.addData("Align With Artifact", bolTurnToArtifact);
+       // telemetry.addData("tx", robotBase.chassisSubsystem.getTargetX());
+        //telemetry.addData("ty", robotBase.chassisSubsystem.getTargetY());
+        //telemetry.addData("ta", robotBase.chassisSubsystem.getTargetArea());
+        //telemetry.addData("Pipeline type", robotBase.chassisSubsystem.getPiplineType());
+        //telemetry.addData("Align With Artifact", bolTurnToArtifact);
         CommandScheduler.getInstance().run();
     }
 }
