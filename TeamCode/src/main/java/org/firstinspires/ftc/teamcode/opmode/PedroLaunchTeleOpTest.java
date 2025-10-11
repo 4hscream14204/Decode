0f9c58aeb0849c45434a547198db734ccaf3b271
PythonMaster245 <connorlithgow@4hscream.org>
2025-10-11 16:04:51 -0500

    Messed with Pinpoint and Pedro.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/PedroLaunchTeleOpTest.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorGoBildaPinpoint;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.pedropathing.tuning.Constants;
+
+import java.util.function.Supplier;
+
+@TeleOp(name = "Pedro Launch TeleOp")
+public class PedroLaunchTeleOpTest extends OpMode {
+    GamepadEx chassis;
+    Follower follower;
+    Pose startPose = new Pose(56, 8, Math.toRadians(0));
+    boolean automatedDrive;
+    Supplier<PathChain> pathChain;
+    double x;
+    double y;
+    double heading;
+    GoBildaPinpointDriver pinpoint;
+    SequentialCommandGroup route;
+    @Override
+    public void init() {
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 56, 8, AngleUnit.RADIANS, Math.toRadians(0)));
+        CommandScheduler.getInstance().reset();
+        chassis = new GamepadEx(gamepad1);
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+        pathChain =()-> follower.pathBuilder() //Lazy Curve Generation
+                .addPath(new Path(new BezierCurve(follower::getPose, new Pose(144, -12, Math.toRadians(-55)))))
+                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(-50), 1))
+                .build();
+
+        chassis.getGamepadButton(GamepadKeys.Button.A)
+                .whenPressed(()-> CommandScheduler.getInstance().schedule(new InstantCommand(()-> follower.followPath(pathChain.get())), new InstantCommand(()->automatedDrive = true)));
+
+        new Trigger(()->automatedDrive && (chassis.wasJustPressed(GamepadKeys.Button.B) || !follower.isBusy()))
+                .whileActiveContinuous(()->CommandScheduler.getInstance().schedule(new InstantCommand(()->follower.startTeleopDrive(true)), new InstantCommand(()->automatedDrive = false)));
+
+        /*route = new SequentialCommandGroup(
+                new InstantCommand(()->new FollowPath(follower, pathChain, true, 1)),
+                new WaitUntilCommand(()->follower.isBusy())
+        );*/
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleopDrive(true);
+    }
+
+    @Override
+    public void loop() {
+        CommandScheduler.getInstance().run();
+        chassis.readButtons();
+        follower.update();
+        pinpoint.setPosX(follower.getPose().getX(), DistanceUnit.INCH);
+        pinpoint.setPosY(follower.getPose().getY(), DistanceUnit.INCH);
+        x = follower.getPose().getX();
+        y = follower.getPose().getY();
+        heading = follower.getHeading();
+        follower.setTeleOpDrive(chassis.getLeftY(), -chassis.getLeftX(), -chassis.getRightX(), true);
+
+        /*pathChain = follower.pathBuilder()
+                .addPath(new BezierCurve(new Pose(x, y, Math.toRadians(0)), new Pose(38.4, 33.4, Math.toRadians(90))))
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(90))
+                .build();*/
+
+        telemetry.addData("position", follower.getPose());
+        telemetry.addData("velocity", follower.getVelocity());
+        telemetry.addData("Heading: ", Math.toDegrees(follower.getHeading()));
+        telemetry.addData("automatedDrive", automatedDrive);
+    }
+}
