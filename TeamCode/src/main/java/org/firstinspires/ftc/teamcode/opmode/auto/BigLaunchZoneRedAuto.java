31d1f141c1acc65b24dd2dbed4b16afbb820d3da
micah <micahmeach@gmail.com>
2025-10-11 19:47:33 -0500

    Finished making routes, arranging then, and naming them

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BigLaunchZoneRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BigLaunchZoneRedAuto.java
@@ -31,16 +31,52 @@ import org.firstinspires.ftc.teamcode.pedropathing.Constants;
         @Override
         public void init() {
             follower = Constants.createFollower(hardwareMap);
-            startPose = new Pose(79.000, 139.000);
+            startPose = new Pose(85, 135);
             CommandScheduler.getInstance().reset();
             goesFromWallToShootPreload = follower.pathBuilder()
                     .addPath(
-                            new BezierCurve(new Pose(79.000, 139.000), new Pose(73.000, 113.000), new Pose(100.000, 125.000)))
-                    .setLinearHeadingInterpolation(Math.toRadians(-90), Math.toRadians(24))
+                            new BezierCurve(new Pose(85, 135), new Pose(85.000, 115.000), new Pose(95, 110)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
+
+            //Closest line//
+            linesUpToIntakeThirdRow = follower.pathBuilder()
+                    .addPath(
+                            new BezierCurve(new Pose(102.903, 123.613), new Pose(79.000, 84.000), new Pose(104.000, 79)))
+                    .setTangentHeadingInterpolation()
+                    .build();
+            intakesThirdRow = follower.pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(104.000, 84.000), new Pose(129.438, 79)))
+                    .setTangentHeadingInterpolation()
+                    .build();
+            shootsArtifacts = follower.pathBuilder()
+                    .addPath(
+                            new BezierCurve(new Pose(129.438, 83.811), new Pose(75.721, 98.049), new Pose(95, 110)))
+                    .setTangentHeadingInterpolation()
+                    .build();
+
+            //Middle line//
+            linesUpWithMiddleRow = follower.pathBuilder()
+                    .addPath(
+                            new BezierCurve(new Pose(102.903, 124.261), new Pose(74.000, 62.000), new Pose(103.000, 54)))
+                    .setTangentHeadingInterpolation()
+                    .build();
+            intakesMiddleRow = follower.pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(103.000, 54), new Pose(127.497, 54)))
+                    .setTangentHeadingInterpolation()
+                    .build();
+            goesToShootArtifacts = follower.pathBuilder()
+                    .addPath(
+                            new BezierCurve(new Pose(127.497, 61.483), new Pose(79.604, 54.688), new Pose(88.018, 116.818), new Pose(95, 110)))
+                    .setTangentHeadingInterpolation()
+                    .build();
+
+            //furthest line//
             linesUpWithFurthestRow = follower.pathBuilder()
                     .addPath(
-                            new BezierCurve(new Pose(100.000, 125.000), new Pose(77.016, 85.753), new Pose(98.049, 38.508)))
+                            new BezierCurve(new Pose(100.000, 125.000), new Pose(77.016, 85.753), new Pose(98.049, 30)))
                     .setConstantHeadingInterpolation(Math.toRadians(0))
                     .build();
             intakesArtifacts = follower.pathBuilder()
@@ -50,37 +86,7 @@ import org.firstinspires.ftc.teamcode.pedropathing.Constants;
                     .build();
             goesToShoot = follower.pathBuilder()
                     .addPath(
-                            new BezierCurve(new Pose(124.908, 38.184), new Pose(99.020, 40.449), new Pose(74.000, 106.000), new Pose(102.903, 124.261)))
-                    .setTangentHeadingInterpolation()
-                    .build();
-            linesUpWithMiddleRow = follower.pathBuilder()
-                    .addPath(
-                            new BezierCurve(new Pose(102.903, 124.261), new Pose(74.000, 62.000), new Pose(103.000, 62.000)))
-                    .setTangentHeadingInterpolation()
-                    .build();
-            intakesMiddleRow = follower.pathBuilder()
-                    .addPath(
-                            new BezierLine(new Pose(103.000, 62.000), new Pose(127.497, 61.483)))
-                    .setTangentHeadingInterpolation()
-                    .build();
-            goesToShootArtifacts = follower.pathBuilder()
-                    .addPath(
-                            new BezierCurve(new Pose(127.497, 61.483), new Pose(79.604, 54.688), new Pose(88.018, 116.818), new Pose(102.903, 123.613)))
-                    .setTangentHeadingInterpolation()
-                    .build();
-            linesUpToIntakeThirdRow = follower.pathBuilder()
-                    .addPath(
-                            new BezierCurve(new Pose(102.903, 123.613), new Pose(79.000, 84.000), new Pose(104.000, 84.000)))
-                    .setTangentHeadingInterpolation()
-                    .build();
-            intakesThirdRow = follower.pathBuilder()
-                    .addPath(
-                            new BezierLine(new Pose(104.000, 84.000), new Pose(129.438, 83.811)))
-                    .setTangentHeadingInterpolation()
-                    .build();
-            shootsArtifacts = follower.pathBuilder()
-                    .addPath(
-                            new BezierCurve(new Pose(129.438, 83.811), new Pose(75.721, 98.049), new Pose(106.000, 123.000)))
+                            new BezierCurve(new Pose(124.908, 38.184), new Pose(99.020, 40.449), new Pose(74.000, 106.000), new Pose(95, 110)))
                     .setTangentHeadingInterpolation()
                     .build();
 
