9e84f25df298777dc2d9432d0a415d1583bf3ed5
AizahR <aizrizwan16@gamil.com>
2025-10-25 14:27:58 -0500

    Added subsystems to RobotBase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/RobotBase.java
@@ -5,9 +5,12 @@ import android.graphics.ColorSpace;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.subsystems.Chassis;
 import org.firstinspires.ftc.teamcode.subsystems.Hood;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.RGBLightSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.Launcher;
 import org.firstinspires.ftc.teamcode.subsystems.Limelight;
@@ -25,17 +28,19 @@ public class RobotBase {
         public SorterServo ejectorLeftSubsystem;
         public SorterServo ejectorRightSubsystem;
         public SorterCamera sorterCameraSubsystem;
+        public Intake Intake;
 
         public RobotBase(HardwareMap hwMap) {
 
-            //intakeSubsystem = new Intake(hwMap.dcMotor.get("intake_motor"));
+            Intake = new Intake(hwMap.dcMotor.get("intake_motor"));
             launcherSubsystem = new Launcher(hwMap.get(DcMotorEx.class, "launcherMotor"));
-           //chassisSubsystem = new Chassis(hwMap.dcMotor.get("rf"), hwMap.dcMotor.get("lf"), hwMap.dcMotor.get("rr"), hwMap.dcMotor.get("lr"), hwMap.get(IMU.class, "imu"));
-           //hoodSubsystem = new Hood(hwMap.get(Servo.class, "hoodServo"));
+           chassisSubsystem = new Chassis(hwMap.dcMotor.get("rf"), hwMap.dcMotor.get("lf"), hwMap.dcMotor.get("rr"), hwMap.dcMotor.get("lr"), hwMap.get(IMU.class, "imu"));
+           hoodSubsystem = new Hood(hwMap.get(Servo.class, "hoodServo"));
            limelightSubsystem = new Limelight(hwMap.get(Limelight3A.class, "limelight"));
-           /*ejectorMiddleSubsystem = new SorterServo(hwMap.servo.get("ejectorMiddle"));
+           ejectorMiddleSubsystem = new SorterServo(hwMap.servo.get("ejectorMiddle"));
            ejectorLeftSubsystem = new SorterServo(hwMap.servo.get("ejectorLeft"));
-           ejectorRightSubsystem = new SorterServo(hwMap.servo.get("ejectorRight"));*/
+           ejectorRightSubsystem = new SorterServo(hwMap.servo.get("ejectorRight"));
+           RGBLightSubsystem = new RGBLightSubsystem(hwMap.servo.get("RGBLightServo"));
 
         }
 }
