6bb1df1d4800136ece498bd7fd0218cdc0ef5a58
micah <micahmeach@gmail.com>
2025-10-25 16:49:41 -0500

    red side SmallLaunchZoneAuto route made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/SmallLaunchZoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/SmallLaunchZoneAuto.java
@@ -23,61 +23,61 @@ import org.firstinspires.ftc.teamcode.pedropathing.Constants;
 
             PathChain lineUpToIntake = follower.pathBuilder()
                     .addPath(
-                            new BezierCurve(new Pose(82.517, 11.002), new Pose(64.719, 80.252), new Pose(104.000, 84.000)))
-                    .setTangentHeadingInterpolation()
+                            new BezierCurve(new Pose(83, 1), new Pose(83, 80), new Pose(104, 84)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain intakeFurthestRow = follower.pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(104.000, 84.000), new Pose(128.144, 84.000)))
-                    .setTangentHeadingInterpolation()
+                            new BezierLine(new Pose(104, 84), new Pose(1, 84)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain goingToShoot = follower.pathBuilder()
                     .addPath(
-                            new BezierCurve(new Pose(128.144, 84.000), new Pose(79.604, 79.604), new Pose(76.369, 12.297)))
-                    .setTangentHeadingInterpolation()
+                            new BezierCurve(new Pose(128, 84), new Pose(80, 80), new Pose(76, 12)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain lineUpToIntakeSecondRow = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(76.369, 12.297), new Pose(74.427, 62.778), new Pose(104.845, 60.000)))
-                    .setTangentHeadingInterpolation()
+                                    new Pose(76, 12), new Pose(74, 63), new Pose(105, 60)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain intakeMiddleRow = follower.pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(104.845, 60.000), new Pose(128.791, 60.000)))
-                    .setTangentHeadingInterpolation()
+                            new BezierLine(new Pose(105, 60), new Pose(129, 60)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain goToShoot = follower.pathBuilder()
                     .addPath(
-                            new BezierCurve(new Pose(128.791, 60.000), new Pose(76.369, 57.600), new Pose(80.575, 13.591)))
-                    .setTangentHeadingInterpolation()
+                            new BezierCurve(new Pose(129, 60), new Pose(76, 58), new Pose(81, 14)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain lineUpToThirdRow = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(80.575, 13.591), new Pose(82.193, 40.449), new Pose(108.728, 36.243)))
-                    .setTangentHeadingInterpolation()
+                                    new Pose(81, 14), new Pose(82, 40), new Pose(109, 36)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain intakeThirdRow = follower.pathBuilder()
-                    .addPath(new BezierLine(new Pose(108.728, 36.243), new Pose(130.085, 36.000)))
-                    .setTangentHeadingInterpolation()
+                    .addPath(new BezierLine(new Pose(109, 36), new Pose(130, 36)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             PathChain lastTimeGoingToShoot = follower.pathBuilder()
                     .addPath(new BezierCurve(
-                            new Pose(130.085, 36.000), new Pose(76.369, 32.360), new Pose(81.546, 9.708)))
-                    .setTangentHeadingInterpolation()
+                            new Pose(130, 36), new Pose(76, 32), new Pose(82, 10)))
+                    .setConstantHeadingInterpolation(0)
                     .build();
 
             route = new SequentialCommandGroup(
-                    new FollowPath(follower, lineUpToIntake, true, 1),
+                    new FollowPath(follower, lineUpToIntake, true, 1)/*,
                     new FollowPath(follower, intakeFurthestRow, true, 1),
                     new FollowPath(follower, goingToShoot, true, 1),
                     new FollowPath(follower, lineUpToIntakeSecondRow, true, 1),
@@ -85,13 +85,14 @@ import org.firstinspires.ftc.teamcode.pedropathing.Constants;
                     new FollowPath(follower, goToShoot, true, 1),
                     new FollowPath(follower, lineUpToThirdRow, true, 1),
                     new FollowPath(follower, intakeThirdRow),
-                    new FollowPath(follower, lastTimeGoingToShoot, true, 1)
+                    new FollowPath(follower, lastTimeGoingToShoot, true, 1)*/
             );
         }
 
         @Override
         public void start() {
             CommandScheduler.getInstance().schedule(route);
+            follower.setStartingPose(new Pose(83, 1, Math.toRadians(0)));
         }
 
         @Override
