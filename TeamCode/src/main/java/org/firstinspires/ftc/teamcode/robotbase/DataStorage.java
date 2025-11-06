98c7bdd22f193d55641c59051d36f1c90489c68f
AizahR <aizrizwan16@gamil.com>
2025-11-05 18:32:09 -0600

    Changed starting Alliance from back from red to blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/DataStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/DataStorage.java
@@ -2,5 +2,5 @@ package org.firstinspires.ftc.teamcode.robotbase;
 
 public class DataStorage {
     public static DecodeEnums.Patterns pattern = DecodeEnums.Patterns.PPG;
-    public static  DecodeEnums.Alliance alliance = DecodeEnums.Alliance.RED;
+    public static  DecodeEnums.Alliance alliance = DecodeEnums.Alliance.BLUE;
 }
