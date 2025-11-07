023313b0b56889c1158c8f96baf4b812eb5d8f02
micah <micahmeach@gmail.com>
2025-11-06 20:40:22 -0600

    Small routes made that only park

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/routes/SmallZoneRedPark.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.pedropathing.routes;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedropathing.tuning.Constants;
+import org.firstinspires.ftc.teamcode.robotbase.DataStorage;
+import org.firstinspires.ftc.teamcode.robotbase.DecodeEnums;
+
+@Autonomous(name = "Small Zone Red Park")
+public class SmallZoneRedPark extends OpMode {
+    Follower follower;
+    SequentialCommandGroup route;
+    PathChain Path1;
+    Pose startPose = new Pose(88, 8, Math.toRadians(0));
+
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        Path1 = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(startPose, new Pose(115.643, 7.754)))
+                .setConstantHeadingInterpolation(0)
+                .build();
+    }
+
+    @Override
+    public void start() {
+        follower.followPath(Path1);
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+    }
+
+    @Override
+    public void stop() {
+        DataStorage.endPosition = follower.getPose();
+        DataStorage.alliance = DecodeEnums.Alliance.BLUE;
+    }
+}
