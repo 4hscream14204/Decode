dfbd228ca5afab1b9a9dbd0432624fc444f48b79
PythonMaster245 <connorlithgow@4hscream.org>
2025-11-11 14:45:06 -0600

    Added @TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TutorialOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TutorialOpMode.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.base.RobotBase;
 
+@TeleOp(name = "Tutorial")
 public class TutorialOpMode extends OpMode {
     RobotBase robotBase;
     @Override
