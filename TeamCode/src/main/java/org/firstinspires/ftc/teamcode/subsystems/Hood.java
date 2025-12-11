2d73040b06d1b52e9cb4e844b36848c557835de4
PythonMaster245 <connorlithgow@4hscream.org>
2025-12-10 20:43:55 -0600

    Messed with ServoEX a bit.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hood.java
@@ -1,20 +1,30 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PwmControl;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.seattlesolvers.solverslib.hardware.SimpleServo;
+import com.seattlesolvers.solverslib.hardware.servos.ServoEx;
 
 public class Hood extends SubsystemBase {
-    Servo hoodServo;
+    ServoEx hoodServo;
     double rangeOfMotion = 300;
-    public Hood(Servo m_hoodServo){
+    double minAngle = 0;
+    double maxAngle = 180;
+    double position;
+
+    public Hood(ServoEx m_hoodServo, HardwareMap hwMap){
         hoodServo = m_hoodServo;
+        hoodServo = new ServoEx(hwMap, "hoodServo", 90, 180);
     }
 
-    public void setPosition(double position){
-        hoodServo.setPosition(position);
+    public void setPosition(double m_position){
+        hoodServo.set(m_position);
     }
 
-    public void autoSetPosition(double angle){
-        hoodServo.setPosition(angle/rangeOfMotion);
+    public void autoSetPosition(double targetAngle){
+        position = (targetAngle - minAngle) / (maxAngle - minAngle);
+        setPosition(position);
     }
 }
