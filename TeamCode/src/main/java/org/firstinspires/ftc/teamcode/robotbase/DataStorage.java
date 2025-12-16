f7b4fb5182b6ea87c78560827fd827290a15a5ca
PythonMaster245 <connorlithgow@4hscream.org>
2025-12-15 21:04:17 -0600

    Odometry distance looking pretty alright.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/DataStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotbase/DataStorage.java
@@ -5,5 +5,5 @@ import com.pedropathing.geometry.Pose;
 public class DataStorage {
     public static DecodeEnums.Patterns pattern = DecodeEnums.Patterns.PPG;
     public static  DecodeEnums.Alliance alliance = DecodeEnums.Alliance.BLUE;
-    public static Pose endPosition = new Pose(0, 0, 0);
+    public static Pose endPosition = new Pose(88, 8, 0);
 }
