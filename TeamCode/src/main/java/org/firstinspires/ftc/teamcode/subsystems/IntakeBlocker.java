d35e4374470404b746d287a363b655c609ddfa9e
SomeGuy568 <potatojack2777@outlook.com>
2026-01-08 18:22:51 -0600

    Wut

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeBlocker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeBlocker.java
@@ -5,6 +5,14 @@ import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 public class IntakeBlocker {
         Servo IntakeBlockerServo;
+
+    public enum Position {
+        OPEN(0),
+        CLOSED(0);
+        public final double value;
+        Position(double m_val){this.value = m_val;}
+    }
+
         public IntakeBlocker(Servo m_intakeBlockerServo) {IntakeBlockerServo = m_intakeBlockerServo;}
 
         public void setPosition(double position) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
@@ -5,6 +5,13 @@ import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 public class Tilt extends SubsystemBase {
     Servo TiltServo;
+
+    public enum Position {
+        ACTIVE(0),
+        INACTIVE(0);
+        public final double value;
+        Position(double m_val){this.value = m_val;}
+    }
     public Tilt(Servo m_tiltServo) {TiltServo = m_tiltServo;}
 
     public void setPosition(double position) {TiltServo.setPosition(position);}
