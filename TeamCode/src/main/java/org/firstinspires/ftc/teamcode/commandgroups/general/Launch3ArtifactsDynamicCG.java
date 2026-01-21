478d15f61c4e28dfc82a4f281d9f9867cd609978
PythonMaster245 <connorlithgow@4hscream.org>
2026-01-20 18:30:47 -0600

    Hopefully fixed the wait until for launching and changed dynamic velocity to use the exceute method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/general/Launch3ArtifactsDynamicCG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/general/Launch3ArtifactsDynamicCG.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.commandgroups.general;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.seattlesolvers.solverslib.command.CommandBase;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.WaitUntilCommand;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.teamcode.robotbase.DataStorage;
 import org.firstinspires.ftc.teamcode.robotbase.DecodeEnums;
 import org.firstinspires.ftc.teamcode.robotbase.RobotBase;
 
-public class Launch3ArtifactsDynamicCG extends SequentialCommandGroup {
+public class Launch3ArtifactsDynamicCG extends CommandBase {
     RobotBase robotBase;
     Pose goalPose;
     Follower follower;
@@ -21,18 +22,14 @@ public class Launch3ArtifactsDynamicCG extends SequentialCommandGroup {
     @Override
     public void initialize(){
         if(DataStorage.alliance == DecodeEnums.Alliance.RED) {
-            addCommands(
-                    new InstantCommand(()->goalPose = new Pose(127.7, 131.7)),
-                    new WaitUntilCommand(() -> robotBase.launcherSubsystemLeft.isAtSpeed(robotBase.limelightSubsystem.getHorizontalDistance(follower, goalPose))),
-                    new Transfer3BallsNoCameraCommandGroup(robotBase)
-            );
+                    goalPose = new Pose(127.7, 131.7);
+                    new WaitUntilCommand(() -> robotBase.launcherSubsystemLeft.isAtSpeed(robotBase.limelightSubsystem.getHorizontalDistance(follower, goalPose)));
+                    new Transfer3BallsNoCameraCommandGroup(robotBase);
         }
         else{
-            addCommands(
-                    new InstantCommand(()->goalPose = new Pose(127.7, 131.7).mirror()),
-                    new WaitUntilCommand(() -> robotBase.launcherSubsystemLeft.isAtSpeed(robotBase.limelightSubsystem.getHorizontalDistance(follower, goalPose))),
-                    new Transfer3BallsNoCameraCommandGroup(robotBase)
-            );
+                    goalPose = new Pose(127.7, 131.7).mirror();
+                    robotBase.launcherSubsystemLeft.isAtSpeed(robotBase.limelightSubsystem.getHorizontalDistance(follower, goalPose));
+                    new Transfer3BallsNoCameraCommandGroup(robotBase);
         }
         super.initialize();
     }
