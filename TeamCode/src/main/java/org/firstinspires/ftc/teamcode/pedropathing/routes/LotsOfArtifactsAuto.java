1193250a1ca02974968519d6138392a15387c41e
(Shared)FTC <4hscream14204@gmail.com>
2026-02-25 15:20:18 -0600

    Auto revert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/routes/LotsOfArtifactsAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/routes/LotsOfArtifactsAuto.java
@@ -31,19 +31,20 @@ public class LotsOfArtifactsAuto extends OpMode {
     RobotBase robotBase;
     Follower follower;
     Pose startPose = new Pose(109.8, 134, Math.toRadians(180));
-    Pose beginLaunch = new Pose(100, 106, Math.toRadians(44));
-    Pose launchPose = new Pose(94, 95, Math.toRadians(47));
+    Pose beginLaunch = new Pose(100, 110, Math.toRadians(49));
+    Pose launchPose = new Pose(87, 87, Math.toRadians(49));
     //Pose launchAftPreloadPose = new Pose(85, 85, Math.toRadians(45));
-    Pose intakeMiddleLineUp = new Pose(110.55, 65, Math.toRadians(0));
-    Pose intakeMiddleRow = new Pose(142.61, 65, Math.toRadians(0));
-    Pose intakeArtifactsFromGate = new Pose(140, 70, Math.toRadians(34));
-    Pose launchAftIntakeFromGate = new Pose(93,91.5,Math.toRadians(45));
-    Pose facingGoalPoint = new Pose(140, 136/*133.5, 139*/);
-    Pose topRowLineUp = new Pose(113, 90.8, Math.toRadians(0));
-    Pose intakeTopRow = new Pose(138, 90.8);
-    Pose park = new Pose(116,69.65,Math.toRadians(23.6));
+    Pose intakeMiddleLineUp = new Pose(94, 60, Math.toRadians(0));
+    Pose intakeMiddleRow = new Pose(129, 60, Math.toRadians(0));
+    Pose pushGate = new Pose(120, 66, Math.toRadians(0));
+    Pose intakeArtifactsFromGate = new Pose(127, 62, Math.toRadians(28));
+    Pose launchAftIntakeFromGate = new Pose(89,76,Math.toRadians(52));
+    Pose facingGoalPoint = new Pose(132, 136/*133.5, 139*/);
+    Pose topRowLineUp = new Pose(96, 82, Math.toRadians(0));
+    Pose intakeTopRow = new Pose(121, 82);
+    Pose park = new Pose(105,65,Math.toRadians(24));
 
-    double dblLaunchVel = 2000;
+    double dblLaunchVel = 1700;
 
     PathChain startPath;
     PathChain launchPath;
@@ -72,18 +73,10 @@ public class LotsOfArtifactsAuto extends OpMode {
                 .addParametricCallback(0, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
                 .addParametricCallback(0, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
                 .addParametricCallback(0, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(1, ()->CommandScheduler.getInstance().schedule(new TransferResetCommandGroup(robotBase)))
-                .addParametricCallback(1, ()->robotBase.intakeSubsystem.intake(-1))
+                .addParametricCallback(0.98, ()->CommandScheduler.getInstance().schedule(new TransferResetCommandGroup(robotBase)))
+                .addParametricCallback(0.98, ()->robotBase.intakeSubsystem.intake(-1))
                 .build();
-        /*launchPath = follower.pathBuilder()
-                .addPath(new BezierLine(beginLaunch, launchPose))
-                .setConstantHeadingInterpolation(Math.toRadians(47))
-                .addParametricCallback(0.25, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(1, ()->CommandScheduler.getInstance().schedule(new TransferResetCommandGroup(robotBase)))
-                .addParametricCallback(1, ()->robotBase.intakeSubsystem.intake(-1))
-                .build();*/
+
 
         intakeMiddleRowPathLineUp = follower.pathBuilder()
                 .addPath(new BezierLine(launchPose, intakeMiddleLineUp))
@@ -98,31 +91,45 @@ public class LotsOfArtifactsAuto extends OpMode {
         middleRowToLaunch = follower.pathBuilder()
                 .addPath(new BezierLine(intakeMiddleRow, launchAftIntakeFromGate))
                 .setHeadingInterpolation(HeadingInterpolator.piecewise(
-                        new HeadingInterpolator.PiecewiseNode(0, 0.5, HeadingInterpolator.linear(intakeArtifactsFromGate.getHeading(), launchAftIntakeFromGate.getHeading())),
-                        new HeadingInterpolator.PiecewiseNode(0.5, 1, HeadingInterpolator.facingPoint(facingGoalPoint))))
+                        new HeadingInterpolator.PiecewiseNode(0, 0.2, HeadingInterpolator.constant(intakeMiddleRow.getHeading())),
+                        new HeadingInterpolator.PiecewiseNode(0.2, 0.4, HeadingInterpolator.linear(intakeArtifactsFromGate.getHeading(), launchAftIntakeFromGate.getHeading())),
+                        new HeadingInterpolator.PiecewiseNode(0.4, 1, HeadingInterpolator.constant(launchAftIntakeFromGate.getHeading()))))
                 //.addParametricCallback(0, ()->new SetAllVelocityCommandGroup(robotBase, 1900))
-                .addParametricCallback(0.85, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.85, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.85, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.9,()->robotBase.intakeSubsystem.intake(1))
+                .addParametricCallback(0.95, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.95, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.95, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.95,()->robotBase.intakeSubsystem.intake(1))
                 .build();
+
+
         launchMiddleToIntake = follower.pathBuilder()
-                .addPath(new BezierLine(launchAftIntakeFromGate, intakeArtifactsFromGate))
+                .addPath(new BezierLine(launchAftIntakeFromGate, pushGate))
+                .setHeadingInterpolation(HeadingInterpolator.piecewise(
+                        new HeadingInterpolator.PiecewiseNode(0, 0.5, HeadingInterpolator.linear(launchAftIntakeFromGate.getHeading(), pushGate.getHeading())),
+                        new HeadingInterpolator.PiecewiseNode(0.5, 1, HeadingInterpolator.constant(pushGate.getHeading()))
+                ))
+                .addParametricCallback(0.5,()->robotBase.intakeSubsystem.intake(-1))
+                .addPath(new BezierLine(pushGate, intakeArtifactsFromGate))
+                .setHeadingInterpolation(HeadingInterpolator.piecewise(
+                        new HeadingInterpolator.PiecewiseNode(0, 0.5, HeadingInterpolator.linear(pushGate.getHeading(), intakeArtifactsFromGate.getHeading())),
+                        new HeadingInterpolator.PiecewiseNode(0.5, 1, HeadingInterpolator.constant(intakeArtifactsFromGate.getHeading()))
+                ))
                 .setConstantHeadingInterpolation(intakeArtifactsFromGate.getHeading())
                 .addParametricCallback(0.1, ()-> CommandScheduler.getInstance().schedule(new TransferResetCommandGroup(robotBase)))
-                .addParametricCallback(0.5,()->robotBase.intakeSubsystem.intake(-1))
                 .build();
+
         intakeFromGateToLaunch = follower.pathBuilder()
                 .addPath(new BezierLine(intakeArtifactsFromGate,launchAftIntakeFromGate))
                 .setHeadingInterpolation(HeadingInterpolator.piecewise(
-                        new HeadingInterpolator.PiecewiseNode(0, 0.5, HeadingInterpolator.linear(intakeArtifactsFromGate.getHeading(), launchAftIntakeFromGate.getHeading())),
-                        new HeadingInterpolator.PiecewiseNode(0.5, 1, HeadingInterpolator.facingPoint(facingGoalPoint))))
+                        new HeadingInterpolator.PiecewiseNode(0, 0.25, HeadingInterpolator.linear(intakeArtifactsFromGate.getHeading(), launchAftIntakeFromGate.getHeading())),
+                        //new HeadingInterpolator.PiecewiseNode(0.25, 1, HeadingInterpolator.facingPoint(facingGoalPoint))))
+                        new HeadingInterpolator.PiecewiseNode(0.25, 1, HeadingInterpolator.constant(launchAftIntakeFromGate.getHeading()))))
                 //.setLinearHeadingInterpolation(intakeArtifactsFromGate.getHeading(),launchAftIntakeFromGate.getHeading())
                 .addParametricCallback(0.1, ()-> CommandScheduler.getInstance().schedule(new TransferResetCommandGroup(robotBase)))
-                .addParametricCallback(0.9, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.9, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.9, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.95, ()->robotBase.intakeSubsystem.intake(1))
+                .addParametricCallback(0.97, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.97, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.97, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.8, ()->robotBase.intakeSubsystem.intake(1))
                 .build();
 
         intakeTopRowPath = follower.pathBuilder()
@@ -139,10 +146,10 @@ public class LotsOfArtifactsAuto extends OpMode {
                 .setHeadingInterpolation(HeadingInterpolator.piecewise(
                         new HeadingInterpolator.PiecewiseNode(0, 0.25, HeadingInterpolator.linear(topRowLineUp.getHeading(), launchAftIntakeFromGate.getHeading())),
                         new HeadingInterpolator.PiecewiseNode(0.25, 1, HeadingInterpolator.facingPoint(facingGoalPoint))))
-                .addParametricCallback(0.85, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.85, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.85, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
-                .addParametricCallback(0.9,()->robotBase.intakeSubsystem.intake(1))
+                .addParametricCallback(0.95, ()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.95, ()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.95, ()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))
+                .addParametricCallback(0.95,()->robotBase.intakeSubsystem.intake(1))
                 .build();
         parking = follower.pathBuilder()
                 .addPath(new BezierLine(launchAftIntakeFromGate,park))
@@ -150,36 +157,40 @@ public class LotsOfArtifactsAuto extends OpMode {
                 .build();
 
         route = new SequentialCommandGroup(
-                new InstantCommand(()->dblLaunchVel = 1850),
+                new InstantCommand(()->dblLaunchVel = 1700),
                 new InstantCommand(()->robotBase.hoodSubsystem.setPosition(Hood.HoodPosition.CLOSE)),
-                new FollowPathCommand(follower, startPath, false, 1),
+                new FollowPathCommand(follower, startPath, true, 1),
                 //new WaitUntilCommand(()->!follower.isBusy()),
                 //new WaitCommand(250),
                 //new FollowPath(follower, launchPath, true, 1),
                 //new WaitUntilCommand(()->!follower.isBusy()),
                 new FollowPathCommand(follower, intakeMiddleRowPathLineUp, true, 1),
-                new InstantCommand(()->dblLaunchVel = 1750),
+
+                new InstantCommand(()->dblLaunchVel = 1740),
                 new FollowPathCommand(follower, intakeMiddleRowPath, true, 1),
                 new FollowPathCommand(follower, middleRowToLaunch, true, 1),
                 new WaitCommand(250),
                 new TransferResetCommandGroup(robotBase),
-                new FollowPathCommand(follower, launchMiddleToIntake,false,0.75),
-                new WaitCommand(1740),
+                new FollowPathCommand(follower, launchMiddleToIntake,true,1),
+                new WaitCommand(400),
                 new FollowPathCommand(follower, intakeFromGateToLaunch, true, 1),
                 new WaitCommand(250),
-                new FollowPathCommand(follower, launchMiddleToIntake, false, 1),
-                new WaitCommand(2000),
+                new FollowPathCommand(follower, launchMiddleToIntake, true, 1),
+                new WaitCommand(400),
                 new FollowPathCommand(follower, intakeFromGateToLaunch, true, 1),
                 new WaitCommand(250),
-                new FollowPathCommand(follower, launchMiddleToIntake, false, 1),
-                new WaitCommand(1750),
+                new FollowPathCommand(follower, launchMiddleToIntake, true, 1),
+                new WaitCommand(600),
                 new FollowPathCommand(follower, intakeFromGateToLaunch, true, 1),
                 new WaitCommand(250),
                 new FollowPathCommand(follower, intakeTopRowPath, true, 1),
                 new FollowPathCommand(follower, launchTopRow, true, 1),
                 new WaitCommand(250),
                 new FollowPathCommand(follower,parking,false,1),
+
+
                 new TransferResetCommandGroup(robotBase)
+
         );
         /*new Trigger(()->follower.getCurrentPathChain() == startPath)
                 .whileActiveContinuous(new SetAllLaunchVelocityCommandGroup(robotBase, robotBase.limelightSubsystem.getHorizontalDistance(follower)));*/
