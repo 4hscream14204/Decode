984857d5173c459f03c28faac1664ea4940619f3
PythonMaster245 <connorlithgow@4hscream.org>
2026-04-13 20:10:43 -0500

    Added red gate auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RedGateAuto.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "Red Gate Auto")
+public class RedGateAuto extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
