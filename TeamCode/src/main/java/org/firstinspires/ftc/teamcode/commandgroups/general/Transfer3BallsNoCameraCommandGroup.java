568408a8a13be20c76730f4b286ff8570ca7fe02
PythonMaster245 <connorlithgow@4hscream.org>
2026-04-13 20:00:38 -0500

    Transfer order changed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/general/Transfer3BallsNoCameraCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/general/Transfer3BallsNoCameraCommandGroup.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.commandgroups.general;
 
 import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.WaitCommand;
 
@@ -10,9 +11,8 @@ import org.firstinspires.ftc.teamcode.subsystems.SorterServo;
 public class Transfer3BallsNoCameraCommandGroup extends SequentialCommandGroup {
     public Transfer3BallsNoCameraCommandGroup(RobotBase robotBase){
         addCommands(
-                new InstantCommand(()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
+                new ParallelCommandGroup(new InstantCommand(()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)), new InstantCommand(()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH))),
                 new InstantCommand(()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
-                new InstantCommand(()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
                 new WaitCommand(300),
                 new TransferResetCommandGroup(robotBase)
         );
