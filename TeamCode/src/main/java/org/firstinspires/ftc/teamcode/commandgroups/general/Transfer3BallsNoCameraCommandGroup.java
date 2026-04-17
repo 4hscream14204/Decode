50d4419f7dc566dbb04b0627e33bdfa8c3cbecec
ElvenGamesB <benelven@outlook.com>
2026-04-16 20:34:37 -0500

    Changed Launch Sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/general/Transfer3BallsNoCameraCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/general/Transfer3BallsNoCameraCommandGroup.java
@@ -11,9 +11,9 @@ import org.firstinspires.ftc.teamcode.subsystems.SorterServo;
 public class Transfer3BallsNoCameraCommandGroup extends SequentialCommandGroup {
     public Transfer3BallsNoCameraCommandGroup(RobotBase robotBase){
         addCommands(
-                new InstantCommand(()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
-                new InstantCommand(()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
                 new InstantCommand(()->robotBase.ejectorMiddleSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
+                new InstantCommand(()->robotBase.ejectorRightSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
+                new InstantCommand(()->robotBase.ejectorLeftSubsystem.setPosition(SorterServo.ServoPosition.LAUNCH)),
                 new WaitCommand(300),
                 new TransferResetCommandGroup(robotBase)
         );
