b1e2508f9ea241fb408f934347c4b15630b448f5
PythonMaster245 <connorlithgow@4hscream.org>
2026-05-14 09:47:10 -0500

    Made branch for new command base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/java/com/arcrobotics/ftclib/hardware/HardwareDevice.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.main.java.com.arcrobotics.ftclib.hardware;
+
+public interface HardwareDevice {
+
+    void disable();
+
+    String getDeviceType();
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/java/com/arcrobotics/ftclib/util/Direction.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.main.java.com.arcrobotics.ftclib.util;
+
+public enum Direction {
+    LEFT, RIGHT, UP, DOWN, FORWARD, BACKWARDS
+}
