70356ab55dd3ad6f671c72c48f40b9ec8bb1221f
AizahR <aizrizwan16@gamil.com>
2026-07-02 19:49:55 -0500

    added Far auto class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/cri/RedFarHPAuto.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.opmode.auto.cri;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous (name = "Red Far HP Auto")
+public class RedFarHPAuto {
+}
