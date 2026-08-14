6a053107b4dd61419a155a18d32c5dd0fe43c2f1
PythonMaster245 <connorlithgow@4hscream.org>
2026-08-13 20:04:10 -0500

    Fixed turret positions! :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/DataStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/DataStorage.java
@@ -6,6 +6,6 @@ public class DataStorage {
     public static DecodeEnums.Patterns pattern = DecodeEnums.Patterns.PPG;
     public static  DecodeEnums.Alliance alliance = DecodeEnums.Alliance.RED;
     public static Pose endPosition = new Pose(94, 10, Math.toRadians(90));
-    public static Pose redGoalPose = new Pose(144, 140);
-    public static Pose blueGoalPose = new Pose(-24, 144);
+    public static Pose redGoalPose = new Pose(144, 148);
+    public static Pose blueGoalPose = new Pose(0, 140);
 }
